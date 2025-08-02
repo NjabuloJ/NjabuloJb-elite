c1db0c1eced37f8b2fdd7fe8bcac0e4eb35f2e1e
NjabuloJ <fanajb65@gmail.com>
2025-08-02 03:39:08 +0200

    Update aira-del.js

--- plugins/aira-del.js
+++ plugins/aira-del.js
@@ -107,8 +107,22 @@ const deleteMessage = async (m, gss) => {
           mentionedJid: [m.sender],
         },
       };
-      gss.sendMessage(m.from, { text: `*${toFancyFont("Message deleted successfully")}`, ...messageOptions });
-    }
+      gss.sendMessage(m.from, { 
+       text: `*${toFancyFont("Message deleted successfully")}`, 
+        ...messageOptions
+      }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
   } catch (error) {
     console.error('Error deleting message:', error);
     const buttons = [
