870846f3ebb1fc9d1cda08358afd75b9c9f40a9b
NjabuloJ <fanajb65@gmail.com>
2025-08-03 00:56:19 +0200

    Update menuc.js

--- plugins/menuc.js
+++ plugins/menuc.js
@@ -92,17 +92,74 @@ const menu = async (m, Matrix) => {
       const messageOptions = {
         viewOnce: true,
         buttons: [
-          { buttonId: `${prefix}download-menu`, buttonText: { displayText: ` Download` }, type: 1 },
-          { buttonId: `${prefix}group-menu`, buttonText: { displayText: ` Group` }, type: 1 },
-          { buttonId: `${prefix}fun-menu`, buttonText: { displayText: ` Fun` }, type: 1 },
-          { buttonId: `${prefix}owner-menu`, buttonText: { displayText: ` Owner` }, type: 1 },
-          { buttonId: `${prefix}ai-menu`, buttonText: { displayText: ` AI` }, type: 1 },
-          { buttonId: `${prefix}anime-menu`, buttonText: { displayText: ` Anime` }, type: 1 },
-          { buttonId: `${prefix}converter-menu`, buttonText: { displayText: ` Converter` }, type: 1 },
-          { buttonId: `${prefix}other-menu`, buttonText: { displayText: ` Other` }, type: 1 },
-          { buttonId: `${prefix}reactions-menu`, buttonText: { displayText: ` Reactions` }, type: 1 },
-          { buttonId: `${prefix}main-menu`, buttonText: { displayText: ` Main` }, type: 1 }
-        ],
+                {
+                  "name": "single_select",
+                  "buttonParamsJson": `{"title":"🔖𝚻𝚫𝚸 𝐅𝚯𝚪 𝚯𝚸𝚵𝚴 𝚳𝚵𝚴𝐔",
+                 "sections":
+                   [{
+                    "title":"😎 🇸​​🇮​​🇱​​🇻​​🇦​-𝛭𝐷 𝛥𝐿𝐿𝛭𝛯𝛮𝑈",
+                    "highlight_label":"🤩 𝛥𝐿𝐿𝛭𝛯𝛮𝑈",
+                    "rows":[
+                      {
+                       "header":"",
+                       "title":"🔰 ᴀʟʟ ᴍᴇɴᴜ",
+                       "description":"🎨🇸​​🇮​​🇱​​🇻​​🇦​-𝛭𝐷 𝛥𝐿𝐿𝛭𝛯𝛮𝑈🎨",
+                       "id":"View All Menu"
+                      },
+                      {
+                        "header":"",
+                        "title":"⬇️ ᴅᴏᴡɴʟᴀᴏᴅᴇʀ ᴍᴇɴᴜ",
+                        "description":"📂𝐒𝚮𝚯𝐖 𝚫𝐋𝐋 𝐃𝚯𝐖𝚴𝐋𝚯𝚫𝐃 𝐅𝚵𝚫𝚻𝐔𝚪𝚵𝐒🗂",
+                        "id":"Downloader Menu"
+                      },
+                      {
+                        "header":"",
+                        "title":"👨‍👨‍👧‍👧ɢʀᴏᴜᴘ ᴍᴇɴᴜ",
+                        "description":"🥵𝐅𝚵𝚫𝚻𝐔𝚪𝚵 𝚻𝚮𝚫𝚻 𝚫𝚪𝚵 𝚯𝚴𝐋𝐘 𝚫𝛁𝚰𝐋𝚫𝚩𝐋𝚵 𝐅𝚯𝚪 𝐆𝚪𝚯𝐔𝚸🥵",
+                        "id":"Group Menu"
+                      },
+                      {
+                        "header":"",
+                        "title":"👨‍🔧 ᴛᴏᴏʟ ᴍᴇɴᴜ",
+                        "description":"🛠 𝐒𝚮𝚯𝐖 𝚳𝚵 𝚻𝚯𝚯𝐋 𝚳𝚵𝚴𝐔",
+                        "id":"Tool Menu"
+                      },
+                      {
+                        "header":"",
+                        "title":"🗿 ᴍᴀɪɴ ᴍᴇɴᴜ",
+                        "description":"📪 𝚩𝚯𝚻 𝚳𝚫𝚰𝚴 𝐂𝚯𝚳𝚳𝚫𝚴𝐃𝐒🗳",
+                        "id":"Main Menu"
+                      },
+                     {
+                        "header":"",
+                        "title":"👨‍💻 ᴏᴡɴᴇʀ ᴍᴇɴᴜ",
+                        "description":"😎𝐅𝚵𝚫𝚻𝐔𝚪𝚵 𝚻𝚮𝚫𝚻 𝚫𝚪𝚵 𝚯𝚴𝐋𝐘 𝐅𝚯𝚪 𝚳𝐘 𝚮𝚫𝚴𝐃𝐒𝚯𝚳𝚵 𝚯𝐖𝚴𝚵𝚪👨‍💼",
+                        "id":"Owner Menu"
+                      },
+                      {
+                        "header":"",
+                        "title":"✨ ᴀɪ ᴍᴇɴᴜ",
+                        "description":"💫 𝐒𝚮𝚯𝐖 𝚳𝚵 𝚫𝚰 𝚳𝚵𝚴𝐔 🎇",
+                        "id":"Ai Menu"
+                      },
+                      {
+                        "header":"",
+                        "title":"🔍sᴇᴀʀᴄʜ ᴍᴇɴᴜ🔎",
+                        "description":"♂️ 𝐒𝚮𝚯𝐖 𝚳𝚵 𝐒𝚵𝚫𝚪𝐂𝚮 𝚳𝚵𝚴𝐔",
+                        "id":"Search Menu"
+                      },
+                      {
+                        "header":"",
+                        "title":"🧚‍♂️ sᴛᴀʟᴋ ᴍᴇɴᴜ",
+                        "description":"👨‍💼 𝐒𝚮𝚯𝐖 𝚳𝚵 𝐒𝚻𝚫𝐋𝐊 𝚳𝚵𝚴𝐔🪆",
+                        "id":"Stalk Menu"
+                      },
+                      {
+                        "header":"",
+                        "title":"🥏 𝚌𝚘𝚗𝚟𝚎𝚛𝚝𝚎𝚛 𝚖𝚎𝚗𝚞",
+                        "description":"🛷 𝐒𝚮𝚯𝐖 𝚳𝚵 𝐂𝚯𝚴𝛁𝚵𝚪𝚻𝚵𝚪 𝚳𝚵𝚴𝐔",
+                        "id":"
+                       ],
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
