29aa11380161b96d6528624031429289604623c2
NjabuloJ <fanajb65@gmail.com>
2025-08-02 21:42:40 +0200

    Update fana-fb.js

--- plugins/fana-fb.js
+++ plugins/fana-fb.js
@@ -194,7 +194,22 @@ const facebook = async (m, Matrix) => {
         mentionedJid: [m.sender],
       },
     };
-    Matrix.sendMessage(m.from, { text: `*${toFancyFont("Njabulo Jb fucked up grabbin’ that video, fam! Try again, you got this!")}`, ...messageOptions }, { quoted: m });
+    Matrix.sendMessage(m.from, { 
+      text: `*${toFancyFont("Njabulo Jb fucked up grabbin’ that video, fam! Try again, you got this!")}`, 
+      ...messageOptions 
+      }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
   }
 };
 
