7d673a02a89510adc5c8bdb3f46ea3b94af9bf05
NjabuloJ <fanajb65@gmail.com>
2025-08-02 23:04:08 +0200

    Update aira-ower.js

--- plugins/aira-ower.js
+++ plugins/aira-ower.js
@@ -8,7 +8,21 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
 
     if (cmd === 'owner') {
         try {
-            await gss.sendContact(m.from, [ownernumber], m);
+            await gss.sendContact(m.from, 
+             [ownernumber],
+             }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
             await m.React("✅");
         } catch (error) {
             console.error('Error sending owner contact:', error);
