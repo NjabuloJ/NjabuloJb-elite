ff70cbb5be8ab3f0d0ce49eac84410739f82aa52
NjabuloJ <fanajb65@gmail.com>
2025-08-05 11:38:36 +0200

    Update img.js

--- plugins/img.js
+++ plugins/img.js
@@ -1,71 +1,19 @@
 import axios from 'axios';
 import config from '../config.cjs';
-import pkg, { prepareWAMessageMedia } from "@whiskeysockets/baileys";
-const { generateWAMessageFromContent, proto } = pkg;
-
-function toFancyFont(text, isUpperCase = false) {
-  const fonts = {
-    a: "ᴀ",
-    b: "ʙ",
-    c: "ᴄ",
-    d: "ᴅ",
-    e: "ᴇ",
-    f: "ғ",
-    g: "ɢ",
-    h: "ʜ",
-    i: "ɪ",
-    j: "ᴊ",
-    k: "ᴋ",
-    l: "ʟ",
-    m: "ᴍ",
-    n: "ɴ",
-    o: "ᴏ",
-    p: "ᴘ",
-    q: "ǫ",
-    r: "ʀ",
-    s: "s",
-    t: "ᴛ",
-    u: "ᴜ",
-    v: "ᴠ",
-    w: "ᴡ",
-    x: "x",
-    y: "ʏ",
-    z: "ᴢ",
-  };
-  const formattedText = isUpperCase ? text.toUpperCase() : text.toLowerCase();
-  return formattedText
-    .split("")
-    .map((char) => fonts[char] || char)
-    .join("");
-}
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const imageCommand = async (m, sock) => {
   const prefix = config.PREFIX;
-  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
-  const query = m.body.slice(prefix.length + cmd.length).trim();
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+const query = m.body.slice(prefix.length + cmd.length).trim();
 
-  const validCommands = ['image', 'img', 'gimage'];
+  const validCommands = ['bing', 'img', 'gimage'];
 
   if (validCommands.includes(cmd)) {
   
     if (!query && !(m.quoted && m.quoted.text)) {
-      const buttons = [
-        {
-          buttonId: `.help`,
-          buttonText: { displayText: `${toFancyFont("Help")}` },
-          type: 1,
-        },
-      ];
-      const messageOptions = {
-        viewOnce: true,
-        buttons,
-        contextInfo: {
-          mentionedJid: [m.sender],
-        },
-      };
-      return sock.sendMessage(m.from, { text: `*${toFancyFont("Please provide some text, Example usage: " + prefix + cmd + " black cats")}`, ...messageOptions });
+      return sock.sendMessage(m.from, { text: `Please provide some text, Example usage: ${prefix + cmd} black cats` });
     }
   
     if (!query && m.quoted && m.quoted.text) {
@@ -75,26 +23,12 @@ const imageCommand = async (m, sock) => {
     const numberOfImages = 5; 
 
     try {
-      const buttons = [
-        {
-          buttonId: `.menu`,
-          buttonText: { displayText: `${toFancyFont("Menu")}` },
-          type: 1,
-        },
-      ];
-      const messageOptions = {
-        viewOnce: true,
-        buttons,
-        contextInfo: {
-          mentionedJid: [m.sender],
-        },
-      };
-      await sock.sendMessage(m.from, { text: `*${toFancyFont("Please wait...")}`, ...messageOptions });
+      await sock.sendMessage(m.from, { text: '*Please wait*' });
 
       const images = [];
 
       for (let i = 0; i < numberOfImages; i++) {
-        const endpoint = `https://apis.davidcyriltech.my.id/googleimage?text=${encodeURIComponent(query)}`;
+        const endpoint = `https://archive-ui.tanakadomp.biz.id/ai/bingimg?text=${encodeURIComponent(query)}`;
         const response = await axios.get(endpoint, { responseType: 'arraybuffer' });
 
         if (response.status === 200) {
@@ -112,21 +46,7 @@ const imageCommand = async (m, sock) => {
       await m.React("✅");
     } catch (error) {
       console.error("Error fetching images:", error);
-      const buttons = [
-        {
-          buttonId: `.report`,
-          buttonText: { displayText: `${toFancyFont("Report")}` },
-          type: 1,
-        },
-      ];
-      const messageOptions = {
-        viewOnce: true,
-        buttons,
-        contextInfo: {
-          mentionedJid: [m.sender],
-        },
-      };
-      await sock.sendMessage(m.from, { text: `*${toFancyFont("Oops! Something went wrong while generating images. Please try again later.")}`, ...messageOptions });
+      await sock.sendMessage(m.from, { text: '*Oops! Something went wrong while generating images. Please try again later.*' });
     }
   }
 };
