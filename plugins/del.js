63700fa47bc1a03db9dcacf8074b2fcc654d6114
NjabuloJ <fanajb65@gmail.com>
2025-08-08 08:04:10 +0200

    Add files via upload

---
+++ plugins/del.js
@@ -0,0 +1,164 @@
+import config from '../config.cjs';
+import pkg, { prepareWAMessageMedia } from "@whiskeysockets/baileys";
+const { generateWAMessageFromContent, proto } = pkg;
+
+function toFancyFont(text, isUpperCase = false) {
+  const fonts = {
+    a: "ᴀ",
+    b: "ʙ",
+    c: "ᴄ",
+    d: "ᴅ",
+    e: "ᴇ",
+    f: "ғ",
+    g: "ɢ",
+    h: "ʜ",
+    i: "ɪ",
+    j: "ᴊ",
+    k: "ᴋ",
+    l: "ʟ",
+    m: "ᴍ",
+    n: "ɴ",
+    o: "ᴏ",
+    p: "ᴘ",
+    q: "ǫ",
+    r: "ʀ",
+    s: "s",
+    t: "ᴛ",
+    u: "ᴜ",
+    v: "ᴠ",
+    w: "ᴡ",
+    x: "x",
+    y: "ʏ",
+    z: "ᴢ",
+  };
+  const formattedText = isUpperCase ? text.toUpperCase() : text.toLowerCase();
+  return formattedText
+    .split("")
+    .map((char) => fonts[char] || char)
+    .join("");
+}
+
+const deleteMessage = async (m, gss) => {
+  try {
+    const botNumber = await gss.decodeJid(gss.user.id);
+    const isCreator = [botNumber, config.OWNER_NUMBER + '@s.whatsapp.net'].includes(m.sender);
+    const prefix = config.PREFIX;
+    const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+    const text = m.body.slice(prefix.length + cmd.length).trim();
+    const img = 'https://files.catbox.moe/84ohd5.jpg';
+    const validCommands = ['del', 'delete'];
+
+    if (validCommands.includes(cmd)) {
+      if (!isCreator) {
+        const buttons = [
+          {
+            buttonId: `.owner`,
+            buttonText: { displayText: `${toFancyFont("Contact Owner")}` },
+            type: 1,
+          },
+        ];
+        const messageOptions = {
+          viewOnce: true,
+          buttons,
+          contextInfo: {
+            mentionedJid: [m.sender],
+          },
+        };
+        return gss.sendMessage(m.from, { text: "* THIS IS AN OWNER COMMAND*", ...messageOptions });
+      }
+
+      if (!m.quoted) {
+        const buttons = [
+          {
+            buttonId: `.help`,
+            buttonText: { displayText: `${toFancyFont("Help")}` },
+            type: 1,
+          },
+        ];
+        const messageOptions = {
+          viewOnce: true,
+          buttons,
+          contextInfo: {
+            mentionedJid: [m.sender],
+          },
+        };
+        return gss.sendMessage(m.from, {
+          image: { url: img },
+          caption: 'Reply to the message you want to delete',
+          ...messageOptions
+        }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
+      }
+
+      const key = {
+        remoteJid: m.from,
+        id: m.quoted.key.id,
+        participant: m.quoted.key.participant || m.quoted.key.remoteJid
+      };
+
+      await gss.sendMessage(m.from, { delete: key });
+
+      const buttons = [
+        {
+          buttonId: `.menu`,
+          buttonText: { displayText: `${toFancyFont("Menu")}` },
+          type: 1,
+        },
+      ];
+      const messageOptions = {
+        viewOnce: true,
+        buttons,
+        contextInfo: {
+          mentionedJid: [m.sender],
+        },
+      };
+      gss.sendMessage(m.from, { 
+       image: { url: img },
+       caption: `*${toFancyFont("Message deleted successfully*")}`, 
+        ...messageOptions
+      }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
+    }
+  } catch (error) {
+    console.error('Error deleting message:', error);
+    const buttons = [
+      {
+        buttonId: `.report`,
+        buttonText: { displayText: `⚠︎${toFancyFont("Report")}` },
+        type: 1,
+      },
+    ];
+    const messageOptions = {
+      viewOnce: true,
+      buttons,
+      contextInfo: {
+        mentionedJid: [m.sender],
+      },
+    };
+    gss.sendMessage(m.from, { text: 'An error occurred while trying to delete the message.', ...messageOptions });
+  }
+};
+
+export default deleteMessage;
