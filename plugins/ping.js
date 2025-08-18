bb0bafd3420ffc91919d2e5df1088807f6026e85
NjabuloJ <fanajb65@gmail.com>
2025-08-18 19:14:47 +0200

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -2,42 +2,6 @@ import config from "../config.cjs";
 import pkg, { prepareWAMessageMedia } from "@whiskeysockets/baileys";
 const { generateWAMessageFromContent, proto } = pkg;
 
-function toFancyFont(text, isUpperCase = false) {
-  const fonts = {
-    a: "ᴀ",
-    b: "ʙ",
-    c: "ᴄ",
-    d: "ᴅ",
-    e: "ᴇ",
-    f: "ғ",
-    g: "ɢ",
-    h: "ʜ",
-    i: "ɪ",
-    j: "ᴊ",
-    k: "ᴋ",
-    l: "ʟ",
-    m: "ᴍ",
-    n: "ɴ",
-    o: "ᴏ",
-    p: "ᴘ",
-    q: "ǫ",
-    r: "ʀ",
-    s: "s",
-    t: "ᴛ",
-    u: "ᴜ",
-    v: "ᴠ",
-    w: "ᴡ",
-    x: "x",
-    y: "ʏ",
-    z: "ᴢ",
-  };
-  const formattedText = isUpperCase ? text.toUpperCase() : text.toLowerCase();
-  return formattedText
-    .split("")
-    .map((char) => fonts[char] || char)
-    .join("");
-}
-
 const ping = async (m, Matrix) => {
   const prefix = config.PREFIX || ".";
   const cmd = m.body.startsWith(prefix)
@@ -105,13 +69,20 @@ const ping = async (m, Matrix) => {
     await Matrix.sendMessage(m.from,{ 
       text:text, 
       ...messageOptions 
-          }, { quoted: {
+}, { quoted: {
             key: {
                 fromMe: false,
                 participant: `0@s.whatsapp.net`,
                 remoteJid: "status@broadcast"
             },
+            message: {
+                contactMessage: {
+                    
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
         } });
+      
      }
   };
                              
